3a609741a9854f9bb70b33f5d3dea8d2fd3d1c67
Fraser Willox <Fraser.Willox@Gov.Scot>
2025-08-15 12:33:33 +0100

    feat: import SG React UI Component Library

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import '@scottish-government/design-system/dist/css/design-system.css';
+
 import "./globals.css";
 
 const geistSans = Geist({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-// import Button from '@scottish-government/designsystem-react/src/components/Button';
-
+import Button from '@scottish-government/designsystem-react/src/components/Button/Button';
 
 
 export default function EPCPage() {
   return (
     <div>
       <h1>EPC Public Register</h1>
+      <Button>Button</Button>
     </div>
   );
 }
