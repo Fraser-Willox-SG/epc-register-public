fbc43a39ebad8d72abac1d174ff0a1935354201f
Fraser Willox <Fraser.Willox@Gov.Scot>
2026-07-27 15:18:54 +0100

    fix: content update. Seperate No Postcode and No RRN content results.

---
+++ src/app/components/NoPostcodeResultsContent.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+
+type NoPostcodeResultsContentProps = {
+  postcode: string;
+};
+
+export function NoPostcodeResultsContent({
+  postcode,
+}: NoPostcodeResultsContentProps) {
+  return (
+    <div className="ds_inset-text">
+      <p>
+        No results found for <strong>{postcode.toUpperCase()}</strong>.
+      </p>
+
+      <p>This could be because the:</p>
+
+      <ul>
+        <li>property or building does not have a certificate</li>
+        <li>certificate has been cancelled or withdrawn</li>
+      </ul>
+
+      <p>
+        If you need a new certificate, go to our{" "}
+        <Link href="/find-advisor" className="ds_link">
+          Find an assessor or advisor
+        </Link>{" "}
+        section. Or{" "}
+        <a href="mailto:epcenquiries@gov.scot" className="ds_link">
+          contact us
+        </a>{" "}
+        if you need more help.
+      </p>
+    </div>
+  );
+}
